423ce2dc868b8d5551a396f0e1d8efb8953ac8c2
sunnykid <sunnykid7@gmail.com>
2024-10-08 17:22:59 +0900

    20241008-3

--- src/Ex01.java
+++ src/Ex01.java
@@ -5,6 +5,7 @@ public class Ex01 {
 		// TODO Auto-generated method stub
 		System.out.println("Hello JAVA!!!");
 		System.out.println("Hello JAVA2!!!");
+		System.out.println("Hello JAVA3!!!");
 	}
 
 }
